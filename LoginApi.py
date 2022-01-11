5c716a2b1763abdfde87f3ce4064065431abaf38
thethe133@naver.com <thethe133@naver.com>
2022-01-11 19:26:57 +0900

    feat : 로그인 기능 구현

---
+++ LoginApi.py
@@ -0,0 +1,20 @@
+from flask import render_template, request, Blueprint, jsonify
+
+from app import db
+
+login_api = Blueprint("login", __name__)
+
+@login_api.route("/login")
+def login_page():
+    return render_template('login.html')
+
+@login_api.route('/login', methods=['POST'])
+def login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    user = db.signup.find_one({'id': id_receive, 'pw': pw_receive})
+    if user is None:
+        return jsonify({'msg': '아이디나 비밀번호를 확인해주세요.'})
+    else:
+        return jsonify({'msg': '로그인되었습니다.'})
