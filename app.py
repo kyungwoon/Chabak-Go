c17ec80954279b4add68f57dd8d862108b390fbd
tacocat3 <yongjusung3351@gmail.com>
2022-01-12 12:37:17 +0900

    jwt 토큰, 페이지 정렬, 비밀번호 해시화 했습니다

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,36 +7,60 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+#JWT 토큰 비밀문자열
+SECRET_KEY = 'CHABAK'
+
+import jwt
+
+#토큰만료시간
+import datetime
+
+#비밀번호 암호화
+import hashlib
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route("/login")
-def login_page():
     return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
+@app.route("/signup")
+def login_page():
+    return render_template('index.html')
+
+###API###
+
+@app.route('/api/login', methods=['POST'])
 def login():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
 
-    user = db.signup.find_one({'id': id_receive, 'pw': pw_receive})
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    user = db.signup.find_one({'id': id_receive, 'pw': pw_hash})
     if user is None:
         return jsonify({'msg': '아이디나 비밀번호를 확인해주세요.'})
     else:
-        return jsonify({'msg': '로그인되었습니다.'})
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
-@app.route('/signup', methods=['POST'])
+        return jsonify({'result': 'success', 'token': token, 'msg' : '로그인되었습니다.'})
+
+
+
+@app.route('/api/signup', methods=['POST'])
 def sign_up():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     name_receive = request.form['name_give']
 
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
     doc = {
         'id': id_receive,
-        'pw': pw_receive,
+        'pw': pw_hash,
         'name': name_receive
     }
     db.signup.insert_one(doc)
